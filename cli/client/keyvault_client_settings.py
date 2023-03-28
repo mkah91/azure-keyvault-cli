9ca40645aff5ec29bac4bcbb0c3b48268a3b3250
KNM2RLZ <marius.knepper2@de.bosch.com>
2023-03-28 18:20:53 +0200

    Use dataclasses

--- cli/client/keyvault_client_settings.py
+++ cli/client/keyvault_client_settings.py
@@ -1,16 +1,28 @@
+from dataclasses import dataclass, field
 from datetime import datetime, timedelta
 import json
 from pathlib import Path
+from typing import Optional
 
 
-class KeyVaultClientSettings:
-    def __init__(self):
-        self.vault_url = None
-        self.auth_record = None
-        self.last_login_time = None
+@dataclass
+class KeyVaultClientSettingsData:
+    vault_url: Optional[str] = None
+    auth_record: Optional[str] = None
+    last_login_time: Optional[datetime] = None
+
+
+class KeyVaultClientSettings(KeyVaultClientSettingsData):
+    def __init__(
+        self,
+        vault_url: Optional[str] = None,
+        auth_record: Optional[str] = None,
+        last_login_time: Optional[datetime] = None,
+    ):
+        super().__init__(vault_url, auth_record, last_login_time)
         self._location = Path.home() / ".azkv" / "settings.json"
+        self._valid_settings_hours = 24
         self._location.parent.mkdir(parents=True, exist_ok=True)
-        self.__valid_settings_hours = 24
 
     def save(self):
         settings_dict = {
@@ -33,7 +45,7 @@ class KeyVaultClientSettings:
         )
 
     def is_valid(self) -> bool:
-        return self._exists() and self._mtime() > (datetime.now() - timedelta(hours=self.__valid_settings_hours))
+        return self._exists() and self._mtime() > (datetime.now() - timedelta(hours=self._valid_settings_hours))
 
     def _mtime(self) -> datetime:
         return datetime.fromtimestamp(self._location.stat().st_mtime)
--- cli/client/keyvault_secret.py
+++ cli/client/keyvault_secret.py
@@ -1,17 +1,13 @@
+from dataclasses import dataclass, field
 from datetime import datetime, timedelta, timezone
 from typing import Optional
 
-from azure.keyvault.secrets import SecretProperties
-
-
+@dataclass
 class Secret:
-    def __init__(self, properties: SecretProperties, value: Optional[str] = None):
-        self.name = properties.name
-        self.expires_on = properties.expires_on
-        self.value = None
-        if value:
-            self.value = value
-        self.__days_before_expiration = 15
+    name: str
+    expires_on: Optional[datetime]
+    value: Optional[str] = field(default=None)
+    _days_before_expiration: int = field(default=15, init=False)
 
     def is_expired(self) -> bool:
         if not self.expires_on:
@@ -21,4 +17,4 @@ class Secret:
     def is_soon_expired(self) -> bool:
         if not self.expires_on or (self.expires_on < datetime.now(timezone.utc)):
             return False
-        return self.expires_on < (datetime.now(timezone.utc) + timedelta(days=self.__days_before_expiration))
+        return self.expires_on < (datetime.now(timezone.utc) + timedelta(days=self._days_before_expiration))
